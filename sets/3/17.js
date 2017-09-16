2ea2684d1c10b71865d53ea6d38c82ecdab1b8a4
Giuseppe Burtini <joe@truephp.com>
2017-09-16 15:51:45 -0700

    Challenge 17. It works now.

--- sets/3/17.js
+++ sets/3/17.js
@@ -1,5 +1,3 @@
-const assert = require('assert');
-
 const aes = require('../../lib/aes');
 const xor = require('../../lib/xor');
 const { chunk, naiveBytesToString, naiveStringToBytes, decodeBase64 } = require('../../lib/stream');
@@ -29,7 +27,8 @@ function challengeSeventeen() {
     // Select at random, one of the 10 strings, then properly CBC encrypt. This should
     // again be interpreted as being a black box web server type interaction.
     const randomIndex = Math.floor(Math.random() * INPUT_STRINGS.length);
-    const string = INPUT_STRINGS[index || randomIndex];
+    if (index === undefined) index = randomIndex;
+    const string = INPUT_STRINGS[index];
 
     return naiveBytesToString(aes.encrypt.cbc(cbcSettings, aes.pkcs7Pad(string)));
   }
@@ -46,71 +45,104 @@ function challengeSeventeen() {
     }
   }
 
+  function recoverOneCharacter(
+    previousBlock,
+    i,
+    before,
+    after,
+    nextBlock,
+    backtrackInvalidValues = []
+  ) {
+    for (
+      let asciiOffset = 1;
+      asciiOffset < 256 && after.length < i; // the whole characterset range AND we have not learned enough yet.
+      asciiOffset++
+    ) {
+      const guessedCharacter = String.fromCharCode(asciiOffset);
+
+      const manipulatedByte = naiveBytesToString(
+        xor.many(
+          naiveStringToBytes(before),
+          naiveStringToBytes(guessedCharacter + after),
+          // expected actual padding.
+          naiveStringToBytes(String.fromCharCode(i).repeat(i))
+        )
+      );
+
+      const testBlocks = previousBlock + manipulatedByte + nextBlock;
+
+      if (oracle(testBlocks)) {
+        if (backtrackInvalidValues.includes(guessedCharacter)) continue;
+        return guessedCharacter;
+      } else if (asciiOffset === 255) {
+        throw new Error(`Failed to decode byte ${i}.`);
+      }
+    }
+    return false;
+  }
+
   function recoverOneBlock(block, previousBlock) {
     const knownCharacters = [];
-    for (let characterOffset = 0; characterOffset < 16; characterOffset++) {
+    let knownInvalid = [];
+    for (let characterOffset = 1; characterOffset <= 16; characterOffset++) {
       const manipulatedBlock = previousBlock.substr(0, previousBlock.length - characterOffset);
-      if (knownCharacters.length < characterOffset - 1) {
-        // Either something went wrong, or we've run out of bytes to decrypt.
-        break;
-      }
-
-      for (
-        let asciiOffset = 1;
-        asciiOffset < 255 && knownCharacters.length <= characterOffset - 1;
-        asciiOffset++
-      ) {
-        const guessedCharacter = String.fromCharCode(asciiOffset);
-
-        const manipulatedByte = naiveBytesToString(
-          xor.many(
-            naiveStringToBytes(previousBlock.substr(-1 * characterOffset)),
-            naiveStringToBytes(
-              guessedCharacter +
-                knownCharacters
-                  .slice()
-                  .reverse()
-                  .join('')
-            ),
-            // expected actual padding.
-            naiveStringToBytes(String.fromCharCode(characterOffset).repeat(characterOffset))
+      try {
+        knownCharacters.push(
+          recoverOneCharacter(
+            manipulatedBlock,
+            characterOffset,
+            previousBlock.substr(-1 * characterOffset),
+            knownCharacters
+              .slice()
+              .reverse()
+              .join(''),
+            block,
+            knownInvalid
           )
         );
-
-        const testBlocks = manipulatedBlock + manipulatedByte + block;
-
-        if (oracle(testBlocks)) {
-          if (characterOffset === 1) {
-            // TODO: it is possible we've got the wrong byte here.
-          }
-          knownCharacters.push(guessedCharacter);
-        } else if (asciiOffset === 255) {
-          throw new Error(`Failed to decode byte ${characterOffset}`);
+        knownInvalid = []; // reset the invalids.
+      } catch (e) {
+        // A backtracking step to handle the chance case; it is possible, for example, for a block to happen to end in [0x01] or [0x02, 0x02].
+        // So we advance in that case, and try to solve the next byte. If we fail, we step back and blacklist the character. There are other
+        // solutions that "work," but it is actually possible but extremely unlikely to get [0x05, 0x05, 0x05, 0x05, 0x05], and this will handle that.
+        if (knownCharacters.length) {
+          knownInvalid.push(knownCharacters[knownCharacters.length - 1]);
+          // console.log('Calling ', knownInvalid, ' invalid and rewinding');
+          delete knownCharacters[knownCharacters.length - 1];
+          characterOffset -= 2;
+        } else {
+          throw e;
         }
       }
     }
+
     return knownCharacters
       .slice()
       .reverse()
       .join('');
   }
 
-  for (let i = 1; i < INPUT_STRINGS.length; i++) {
-    console.log('\nMessage', i);
+  for (let i = 0; i < INPUT_STRINGS.length; i++) {
     const ciphertext = cipher(i);
     const chunkedCiphertext = chunk(ciphertext, 16).map(j => j.join(''));
-    console.log(chunkedCiphertext.length);
     let plaintext = '';
     for (let chunkIndex = 0; chunkIndex < chunkedCiphertext.length; chunkIndex++) {
-      plaintext += recoverOneBlock(
-        chunkedCiphertext[chunkIndex],
-        chunkedCiphertext[chunkIndex - 1] || cbcSettings.iv
-      );
+      try {
+        plaintext += recoverOneBlock(
+          chunkedCiphertext[chunkIndex],
+          chunkedCiphertext[chunkIndex - 1] || cbcSettings.iv
+        );
+      } catch (e) {
+        // rethrow as a user friendly error.
+        throw new Error(
+          `Failed (${e.message}) to decrypt text ${i} - before failing, got: ${plaintext}.`
+        );
+      }
     }
     console.log('Plaintext', i, 'recovered:', plaintext);
   }
 }
-// challengeSeventeen();
+challengeSeventeen();
 
 module.exports = {
   run: challengeSeventeen,
