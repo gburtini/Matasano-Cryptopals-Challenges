d7be6cc70c4dc361c1f41570c749628f78b3516c
Giuseppe Burtini <joe@truephp.com>
2017-09-23 12:03:38 -0700

    Implementing aesCTR in our aes library properly.

--- sets/3/18.js
+++ sets/3/18.js
@@ -1,6 +1,5 @@
 const aes = require('../../lib/aes');
-const xor = require('../../lib/xor');
-const { chunk, decodeBase64, naiveBytesToString, naiveStringToBytes } = require('../../lib/stream');
+const { decodeBase64 } = require('../../lib/stream');
 const { arbitraryPad } = require('../../lib/aes');
 
 const INPUT = decodeBase64(
@@ -10,22 +9,8 @@ const INPUT = decodeBase64(
 
 function challengeEighteen() {
   const nonceCharacter = arbitraryPad('', 8, String.fromCharCode(0));
-  const ctrSettings = { key: 'YELLOW SUBMARINE' };
-  const chunkedPlaintext = chunk(INPUT, 16);
-  const chunkedCiphertext = [];
-
-  for (let counter = 0; counter < chunkedPlaintext.length; counter++) {
-    const counterCharacter = arbitraryPad(String.fromCharCode(counter), 8, String.fromCharCode(0));
-    const cipheredCounter = aes.encrypt.ecb(ctrSettings, `${nonceCharacter}${counterCharacter}`);
-
-    const appropriateLengthCipheredCounter = naiveStringToBytes(
-      naiveBytesToString(cipheredCounter).substr(0, chunkedPlaintext[counter].length)
-    );
-
-    chunkedCiphertext.push(xor.many(chunkedPlaintext[counter], appropriateLengthCipheredCounter));
-  }
-
-  return chunkedCiphertext.map(naiveBytesToString).join('');
+  const ctrSettings = { key: 'YELLOW SUBMARINE', nonce: nonceCharacter };
+  return aes.decrypt.ctr(ctrSettings, INPUT);
 }
 
 module.exports = {
