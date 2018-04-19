efa8a650526aa0152c1a91cf40d8388b16ea6cb0
Giuseppe Burtini <giuseppe@burtini.ca>
2018-04-18 17:11:55 -0700

    Challenge 20 complete.

--- sets/3/20.js
+++ sets/3/20.js
@@ -1,6 +1,6 @@
 const aes = require('../../lib/aes');
 const attacks = require('../../lib/attacks');
-const { decodeBase64, naiveStringToBytes, naiveBytesToString, chunk } = require('../../lib/stream');
+const { decodeBase64, chunk } = require('../../lib/stream');
 
 const fs = require('fs');
 const path = require('path');
@@ -23,6 +23,10 @@ function challengeTwenty() {
     .filter(i => i);
 
   const minLength = Math.min(...INPUTS.map(i => i.length));
+
+  // NOTE: we're forced to truncate the inputs here to the minimum length because of how `attacks.breakRepeatingKey` works
+  // but I wonder if there isn't an obvious way to solve the rest of it once we've solved some. In any case, there's no reason
+  // we need to do the minimum length; some longer length could definitely be done (so long as there's numerous samples).
   const input = INPUTS.map(i => i.substring(0, minLength));
 
   return chunk(attacks.breakRepeatingKeyXor(input.join(''), minLength, minLength + 1), minLength)
