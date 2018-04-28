a6f5f84128f14c88f3ed7afcd8c1c23976ec0426
Giuseppe Burtini <giuseppe@burtini.ca>
2018-04-27 17:10:43 -0700

    Challenge 23 works. Took a lot of dealing with JS bitshiftery, but we're there!

--- sets/3/23.js
+++ sets/3/23.js
@@ -1,7 +1,21 @@
-const assert = require('assert');
 const mt19937 = require('./../../lib/mt19937');
+const assert = require('assert');
 
-function challenge() {}
+function challenge() {
+  const rng = mt19937.create(108123);
+
+  // generate a candidate sequence that is "large enough"
+  const candidateSequence = Array(624)
+    .fill(0)
+    .map(rng);
+
+  const candidateRng = mt19937.clone(candidateSequence);
+
+  assert(candidateRng() === rng());
+  assert(candidateRng() === rng());
+  assert(candidateRng() === rng());
+  return { cloned: candidateRng(), original: rng() };
+}
 
 module.exports = {
   run: challenge,
