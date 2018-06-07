6bd34f6687689edbfe4938753a1ca98956dfcc36
Giuseppe Burtini <giuseppeburtini@gmail.com>
2018-06-07 10:21:56 -0700

    Completed implementation of 28.

---
+++ sets/4/28.js
@@ -0,0 +1,33 @@
+const sha1 = require('js-sha1');
+const assert = require('assert');
+
+function challenge() {
+  const secretKey = 'YELLOW SUBMARINE';
+  function sha1Mac(message) {
+    return sha1(`${secretKey}${message}`);
+  }
+  function checkMessage({ message, mac }) {
+    return sha1Mac(message) === mac;
+  }
+  function generate(message) {
+    return {
+      message,
+      mac: sha1Mac(message),
+    };
+  }
+
+  const serverResponse = generate('hello');
+  const manipulatedResponse = {
+    mac: serverResponse.mac,
+    message: 'goodbye',
+  };
+
+  assert(checkMessage(manipulatedResponse) === false);
+  assert(checkMessage(serverResponse) === true);
+  return true;
+}
+
+module.exports = {
+  run: challenge,
+  describe: 'Implement a SHA1 keyed MAC',
+};
