4c9dc204e9e1b001a20f0d64f68b6f083158de1f
Giuseppe Burtini <giuseppeburtini@gmail.com>
2018-06-08 20:44:10 -0700

    Add initial implementation of 27. It doesn't work yet.

--- sets/4/27.js
+++ sets/4/27.js
@@ -1,7 +1,7 @@
 const aes = require('../../lib/aes');
 const xor = require('./../../lib/xor');
 
-const { naiveBytesToString, naiveStringToBytes } = require('../../lib/stream');
+const { naiveBytesToString, naiveStringToBytes, chunk } = require('../../lib/stream');
 const assert = require('assert');
 
 function isAscii(str) {
@@ -22,11 +22,11 @@ function challenge() {
       throw new Error('All blocks should be exactly 16 bytes long.');
     }
 
-    return aes.encrypt.cbc(`${b1}${b2}${b3}`, cbcSettings);
+    return aes.encrypt.cbc(cbcSettings, `${b1}${b2}${b3}`);
   }
 
   function decryptAndError(message) {
-    const plaintext = aes.decrypt.cbc(message, cbcSettings);
+    const plaintext = aes.decrypt.cbc(cbcSettings, message);
     const highAscii = !isAscii(plaintext); // if high ASCII, throw an error.
     if (highAscii) {
       const error = new Error('High ASCII found.');
@@ -36,16 +36,42 @@ function challenge() {
     return plaintext;
   }
 
-  const ciphertext = encrypt('1234567812345678', 'abcdefghabcdefgh', '!@#$%^&*!@#$%^&*');
-  // TODO: attack with bitflipping to substitute block 2 with the 0 block... make sure we cause high ascii.
+  function bruteforceByteExchange(from, to) {
+    for (let i = 1; i < 256; i++) {
+      if ((from.charCodeAt(0) ^ i) === to.charCodeAt(0)) {
+        return i;
+      }
+    }
+    throw new Error(`Unable to find byte exchange for ${from} to ${to}.`);
+  }
+  function swapCharacterInNextBlock(block, location, from, to) {
+    const clonedArray = block.slice(0);
+    clonedArray[location] = String.fromCharCode(
+      clonedArray[location].charCodeAt(0) ^ bruteforceByteExchange(from, to)
+    );
+    return clonedArray;
+  }
+
+  const ciphertext = chunk(
+    naiveBytesToString(encrypt('1234567812345678', 'abcdefghabcdefgh', '!@#$%^&*!@#$%^&*')),
+    16
+  );
+  // TODO: attack with bitflipping to substitute block 2 with the 0 block.
   // using the zero block works because it assures that the first ciphertext block and the one after the
   // zero block are both encrypted (XOR'd) with just the key... same key means b1 XOR b3 == key.
+  ciphertext[0] = swapCharacterInNextBlock(ciphertext[0], 0, 'a', String.fromCharCode(0));
+  ciphertext[0] = swapCharacterInNextBlock(ciphertext[0], 1, 'b', String.fromCharCode(0));
+  ciphertext[0] = swapCharacterInNextBlock(ciphertext[0], 2, 'c', String.fromCharCode(0));
+  ciphertext[0] = swapCharacterInNextBlock(ciphertext[0], 3, 'd', String.fromCharCode(0));
+
+  console.log(decryptAndError(ciphertext));
+  const attackedText = 'bogon';
   try {
-    decryptAndError(attackedText);
+    return decryptAndError(attackedText);
   } catch (e) {
     // we have high ascii.
     const invalidPlaintext = e.plaintext;
-    const chunks = chunk(invalidPlaintext);
+    const chunks = chunk(invalidPlaintext, 16);
     const recoveredKey = xor.byte(chunks[0], chunks[2]);
     // TODO: validate that the key is right.
     return recoveredKey;
